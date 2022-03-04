37b0a9e58c466d2c567d04b46e8fb7a8787064a5
jainil1511 <jainildarji007@gmail.com>
2022-03-04 23:45:49 +0530

    04-03 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navigation from './components/Navigation';
-import Products from './pages/Products';
+import ProductsPage from './pages/ProductsPage';
 import Cart from './pages/Cart';
 import SingleProduct from './pages/SingleProduct';
 
@@ -14,7 +14,7 @@ const App =() => {
            <Routes>
             <Route path="/" element ={<Home/>} exact></Route>
             {/* <Route path="/about" element={<About/>}></Route> */}
-            <Route path="/products" exact element={<Products/>}></Route>
+            <Route path="/products" exact element={<ProductsPage/>}></Route>
             <Route path="/products/:_id" exact element={<SingleProduct/>}></Route>
             <Route path="/cart" element={<Cart/>}></Route>
            </Routes>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,34 +1,37 @@
-import {Link,useParams} from 'react-router-dom';
+import {Link,useParams,useNavigate } from 'react-router-dom';
 import {React,useState,useEffect} from 'react'
 
 const SingleProduct = () => {
 const [product,setProduct] = useState({});
 const params = useParams();
+const navigate = useNavigate();
 useEffect(() => {
     
     fetch(`https://ecom-rest-apis.herokuapp.com/api/products/${params._id}`)
     .then(res => res.json())
     .then(product => {
+      setProduct(product)
         console.log(product);
     })
-}, []);
+}, [params._id]);
 
 
   return (
     <div className='container mx-auto mt-12'>
-        <Link to="/">
-        <button className='mb-12 font-bold'>Back</button>
+      
+        <button className='mb-12 font-bold' onClick={() => {navigate('/');}}>Back</button>
+       
         <div className='flex'>
-        <img src="/images/peproni.png" alt="pizza" />
+        <img src={product.image} alt="pizza" />
         <div className='ml-16'>
-            <h1 className='text-xl font-bold'>Hawana Specil</h1>
-            <div className='text-md'>Small</div>
-            <div className='font-bold mt-2'>₹ 400</div>
+            <h1 className='text-xl font-bold'>{product.name}</h1>
+            <div className='text-md'>{product.size}</div>
+            <div className='font-bold mt-2'>₹ {product.price}</div>
             <button className='bg-yellow-500 py-1 px-8 rounded-full font-bold mt-4'>Add to Cart</button>
         </div>
         </div>
         
-        </Link>
+     
     </div>
   )
 }
